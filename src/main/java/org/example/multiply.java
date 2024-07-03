39d12924eb12fd921567ccd60fca3882c8faa48c
Nidhi Yadav <nidhi.yadav@aliceblueindia.com>
2024-07-03 12:20:00 +0530

    adding multiply file

---
+++ src/main/java/org/example/multiply.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class multiply {
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=9;
+        System.out.println("this is the multiplication "+ a*b);
+
+    }
+}
